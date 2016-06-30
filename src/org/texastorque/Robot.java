42e8f00caa4b40f074086ee444eddc34721c88a6
DanielHurin <DanielHurin@gmail.com>
2016-06-30 13:18:54 -0500

    initialized robot repository

---
+++ src/org/texastorque/Robot.java
@@ -0,0 +1,32 @@
+package org.texastorque;
+
+import org.texastorque.torquelib.base.TorqueIterative;
+
+public class Robot extends TorqueIterative{
+
+	@Override
+	public void robotInit() {
+		
+	}
+	
+	@Override
+	public void autonomousInit() {
+		
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+	
+	@Override
+	public void teleopInit() {
+		
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+		
+	}
+	
+}
